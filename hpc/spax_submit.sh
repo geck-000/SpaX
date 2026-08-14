1e3e0d0d2e11f8ad5af4f446b0388c8309dabe5a
Giacomo Stirpe <giacomostirpe@gmail.com>
2026-08-14 16:45:18 +0300

    Raise the weibull_layer solve allocation after out-of-memory failures

--- hpc/spax_submit.sh
+++ hpc/spax_submit.sh
@@ -37,7 +37,7 @@ torsion_homog|rve_torsion_homog.csv|TORH|postprocess_torsion.sh|results_torsion_
 torsion_layer|rve_torsion_layer.csv|TORL|postprocess_torsion.sh|results_torsion_layer_K.csv|2|64G
 torsion_layer_homog|rve_torsion_layer_homog.csv|TORLH|postprocess_torsion.sh|results_torsion_layer_homog_K.csv|2|64G
 weibull|rve_weibull.csv|WBL|postprocess_weibull_scf.sh|results_weibull_scf.csv|1|32G
-weibull_layer|rve_weibull_layer.csv|WBLL|postprocess_weibull_scf.sh|results_weibull_layer_scf.csv|1|32G
+weibull_layer|rve_weibull_layer.csv|WBLL|postprocess_weibull_scf.sh|results_weibull_layer_scf.csv|1|180G
 nlgeom_layer|rve_nlgeom_layer.csv|NLGL|postprocess_nlgeom.sh|results_nlgeom_layer.csv|1|32G
 eringen_layer|rve_eringen_layer.csv|ERGL|postprocess_firstorder.sh|results_eringen_layer.csv|2|64G
 eringen_layer_homog|rve_eringen_layer_homog.csv|ERGLH|postprocess_firstorder.sh|results_eringen_layer_homog.csv|2|64G
